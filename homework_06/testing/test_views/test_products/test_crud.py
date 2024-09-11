560fcde049756d7dcba0ce15657cbaa3cfc37689
Dgonede <dgonegonee@gmail.com>
2024-09-11 13:50:59 +0300

    Pytest add

--- homework_06/testing/test_views/test_products/test_crud.py
+++ homework_06/testing/test_views/test_products/test_crud.py
@@ -2,5 +2,5 @@ from app.views.products.crud import ProductsStorage
 
 class TestProductStorage:
     def test_get(self):
-        storage = ProductsStorage()
+        storage = ProductsStorage() #pylint:disable=use-implicit-booleaness-not-comparison
         assert storage.get() == []
